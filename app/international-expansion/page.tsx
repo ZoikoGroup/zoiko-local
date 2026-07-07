97e4196e959a7146e666ab9f7c3fbd573171ac1b
Lakhan Rahela <rahelalakhan@gmail.com>
2026-07-07 18:25:24 +0530

    Develop two new pages

---
+++ app/international-expansion/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Internationalexpansion from './Internationalexpansion';
+
+export default function () {
+  return (
+    <>
+      <Internationalexpansion/>
+    </>
+  )
+}
